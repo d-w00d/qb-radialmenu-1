b70fc3648458c69930fc78886847fbcd94beed5f
Destiny 33 <99442662+DestinyGGS@users.noreply.github.com>
2022-10-09 01:35:27 -0400

    Added Folder

---
+++ qb-radialmenu/server/main.lua
@@ -0,0 +1,11 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+RegisterServerEvent('json:dataStructure')
+AddEventHandler('json:dataStructure', function(data)
+    -- ??
+end)
+
+RegisterServerEvent('qb-radialmenu:trunk:server:Door')
+AddEventHandler('qb-radialmenu:trunk:server:Door', function(open, plate, door)
+    TriggerClientEvent('qb-radialmenu:trunk:client:Door', -1, plate, door, open)
+end)
